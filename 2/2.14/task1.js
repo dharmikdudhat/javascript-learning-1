0d9e2e55a98af6955b4751aaea8c042a939619bd
dharmik <dharmik.dudhat.ait@gmail.com>
2023-09-12 15:35:18 +0530

    2.14

---
+++ 2/2.14/task1.js
@@ -0,0 +1,9 @@
+let browser = "chrome";
+
+if (browser =="edge"){
+    console.log("not good");
+}else if(browser == "chrome" || browser == "firefox" || browser == "safari" || browser == "opera" ){
+    console.log("good");
+}else{
+    console.log("nothing matters");
+}
