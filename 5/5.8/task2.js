e6a7b2a029aed9a583178210e4076ca8416e1e4f
dharmik <dharmik.dudhat.ait@gmail.com>
2023-09-14 15:52:40 +0530

    5.8

---
+++ 5/5.8/task2.js
@@ -0,0 +1,5 @@
+let messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+  ];//we can use weakmap to store the date here
