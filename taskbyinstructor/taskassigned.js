c3bdc111138f818c996c72bf28d7b018465cda94
dharmik <dharmik.dudhat.ait@gmail.com>
2023-10-04 17:32:05 +0530

    task

--- taskbyinstructor/taskassigned.js
+++ taskbyinstructor/taskassigned.js
@@ -1,24 +1,37 @@
-// <!-- Create function that Accepts the String as a input and return the Array of Object sorted by the number of count.
-// Input: Abra ka dabra
-// Output1: Print Total input string length
-// Output2: [{ a: 5 }, { b: 2 }, { r: 2 }, { d: 1 }, { k: 1 }]; -->
-function task(str){
-                    console.log(str.length);
-                    
-
-                    //   for (let i of str){
-                            
-                    //   let obj = new Object{
-                    //     char : i,
+/* Task 1
+Create function that Accepts the String as a input and return the Array of Object sorted by the number of count.
+Input: Abra ka dabra
+Output1: Print Total input string length
+Output2: [{ a: 5 }, { b: 2 }, { r: 2 }, { d: 1 }, { k: 1 }];
+Time to complete this task : 30 mins */
 
 
-                    //   };
-                    //   }
+function countNumber(s) {
 
-                        
-                    
-                    
+    let newString = s.toLowerCase();
 
-                }
+    let arr = [...newString];
 
-                task("abra ka dabra");
+    let newObj = {};
+    
+    let newArrOfObj = [];
+
+    for (let values of arr) {
+        
+        if (values !== ' ') {
+
+            !newObj[values] ? newObj[values] = 1 : newObj[values]++;
+
+        }
+    }
+    newArrOfObj.push(newObj);
+
+    return newArrOfObj.sort((a, b) => b - a);
+
+}
+
+
+
+let a = "Abra ka dabra";
+console.log(a.length);
+console.log(countNumber(a));
