36efeb00c16ee7b8d9814fe765e850ad87c4d628
dharmik <dharmik.dudhat.ait@gmail.com>
2023-10-10 09:45:09 +0530

    changes

--- taskbyinstructor/arrayexercises/missingnumfromsequentialarray.js
+++ taskbyinstructor/arrayexercises/missingnumfromsequentialarray.js
@@ -1,15 +1,18 @@
-
-
 // Write a function to find the missing number in an array of sequential numbers.
 
 function missing(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] !== arr[i + 1] - 1) {
-        console.log(arr[i] + 1);
-       }// else if (arr[i] !== arr[i + 2] - 2) {
-      //   console.log(arr[i] + 2);
-      // }
+  let arraymis = [];
+  let actual = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != arr[i - 1]) {
+      let x = arr[i] - arr[i - 1];
+      let y = 1;
+      while (y < x) {
+        arraymis.push(arr[i - 1] + 1);
+      }
     }
   }
-  let array1 = [1, 2, 3, 5, 7, 8, 9, 10, 13, 14];
-  console.log(missing(array1));
+  return arraymis;
+}
+let array1 = [1, 2, 3, 5, 7, 8, 9, 10, 13, 14];
+console.log(missing(array1));
