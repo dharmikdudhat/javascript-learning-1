c907b1def3835c203b3950ea688936434f8601c9
dharmik <dharmik.dudhat.ait@gmail.com>
2023-10-18 11:31:51 +0530

    async and await instead pf promise and .then

---
+++ javascriptinfo/11/11.8/task1.js
@@ -0,0 +1,31 @@
+/* Rewrite this example code from the chapter Promises chaining using async/await instead of .then/catch:
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    });
+}
+
+loadJson('https://javascript.info/no-such-user.json')
+  .catch(alert); // Error: 404
+solution
+The notes are below the code:
+ */
+async function loadJson(url) { // (1)
+  let response = await fetch(url); // (2)
+
+  if (response.status == 200) {
+    let json = await response.json(); // (3)
+    return json;
+  }
+
+  throw new Error(response.status);
+}
+
+loadJson('https://javascript.info/no-such-user.json')
+  .catch(alert); // Error: 404 (4)
