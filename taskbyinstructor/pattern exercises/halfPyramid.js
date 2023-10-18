78c987dbcd7f45f396aa71f93d1f4186a08b6ff2
dharmik <dharmik.dudhat.ait@gmail.com>
2023-10-18 14:29:05 +0530

    half pyramid

--- taskbyinstructor/pattern exercises/halfPyramid.js
+++ taskbyinstructor/pattern exercises/halfPyramid.js
@@ -1,12 +1,10 @@
 function hollowRectangle(a, b) {
   for (i = 1; i <= a; i++) {
     let row = "";
-    for (j = 1; j <= b; j++) {
-      if (i === 1 || i === a || j === 1 || j === b) {
+    for (j = 1; j <= i; j++) {
+      
         row += "*";
-      } else {
-        row += " ";
-      }
+    
     }
     console.log(row);
   }
